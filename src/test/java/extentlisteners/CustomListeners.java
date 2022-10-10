0d59202d3cec500fb859a7a49bdbf9084ddb8249
vikram phatangare <vikramgp178@gmail.com>
2022-10-10 10:00:24 +0530

    my commits

--- src/test/java/extentlisteners/CustomListeners.java
+++ src/test/java/extentlisteners/CustomListeners.java
@@ -3,7 +3,11 @@ package extentlisteners;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Date;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -11,10 +15,16 @@ import org.testng.ITestResult;
 import com.aventstack.extentreports.ExtentReports;
 
 import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
 
 public class CustomListeners implements ITestListener{
 	
+	WebDriver driver;
 	public InputStream ins;
+	public static Allure allure;
+	static Date d = new Date();
+	static String fileName ="\\Failed_Screenshot_" + d.toString().replace(":", "_").replace(" ", "_")+ ".jpg";
+	static String path =  System.getProperty("user.dir")+fileName;
 //	static String fileName = "V2E_Test_Report_" + d.toString().replace(":", "_").replace(" ", "_") + ".html";
 
 	/*private static ExtentReports extent = ExtentManager
@@ -27,7 +37,10 @@ public class CustomListeners implements ITestListener{
     }		
 
     public void onStart(ITestContext arg0) {					
-        // TODO Auto-generated method stub				
+        // TODO Auto-generated method stub		
+	/*	String param = (String) ((ITestResult) arg0).getParameters()[0];
+		allure = Allure*/
+
         		
     }		
 
@@ -40,12 +53,23 @@ public class CustomListeners implements ITestListener{
         // TODO Auto-generated method stub				
     	
 		try {
-			ins = new FileInputStream("C:\\Users\\VikramPhatangare\\Downloads\\20220822_220656.jpg");
-		} catch (FileNotFoundException e) {
+			
+			//ins = new FileInputStream(path);
+			
+			System.out.println("*** Test execution " + arg0.getMethod().getMethodName() + " failed...");
+			System.out.println(arg0.getMethod().getMethodName() + " failed!");
+
+			ITestContext context = arg0.getTestContext();
+			driver = (WebDriver) context.getAttribute("driver");
+
+			// attach screenshots to report
+			saveFailureScreenShot(driver);
+			
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Allure.attachment("Screenshot", ins); 		
+		//Allure.attachment("Screenshot", ins); 		
     }		
 
     public void onTestSkipped(ITestResult arg0) {					
@@ -62,5 +86,10 @@ public class CustomListeners implements ITestListener{
         // TODO Auto-generated method stub				
         		
     }		
+    
+    @Attachment
+	public byte[] saveFailureScreenShot(WebDriver driver) {
+		return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+	}
 
 }
